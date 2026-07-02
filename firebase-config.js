716b77a3385696beeaaa6537f23cd37159e02e6b
orlandobentes1605-dev <orlandobentes1605@gmail.com>
2026-07-02 16:12:14 -0400

    Add Firebase Analytics to configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAC3xFRezQ-st2JCmUyCG7xMZAUZ5gIjyA",
-  authDomain: "sistemafiliacao-team-as.firebaseapp.com",
-  projectId: "sistemafiliacao-team-as",
-  storageBucket: "sistemafiliacao-team-as.firebasestorage.app",
-  messagingSenderId: "727325899527",
-  appId: "1:727325899527:web:3b4105e30e1f901775e3a2",
-  measurementId: "G-RWTHQZPR3W"
+    apiKey: "AIzaSyAC3xFRezQ-st2JCmUyCG7xMZAUZ5gIjyA",
+    authDomain: "sistemafiliacao-team-as.firebaseapp.com",
+    projectId: "sistemafiliacao-team-as",
+    storageBucket: "sistemafiliacao-team-as.firebasestorage.app",
+    messagingSenderId: "727325899527",
+    appId: "1:727325899527:web:3b4105e30e1f901775e3a2",
+    measurementId: "G-RWTHQZPR3W"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
